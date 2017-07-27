dca5c42aa3ec7d042e6ba957dbecb1e05741306b
Douglas Adamson <douglas.adamson@hc2158.local>
2017-07-27 14:09:08 -0600

    Version 1

---
+++ App.R
@@ -0,0 +1,8 @@
+# Test
+#ticker <- 'AAPL'
+#myYahooQuote <- yahooQuote(ticker)
+#myYahooBatch <- yahooBatch()
+#plotTicker("AAPL", "2017-01-01", "2017-07-17")
+
+# Build a Shiny App
+shinyApp(ui = ui, server = server)
