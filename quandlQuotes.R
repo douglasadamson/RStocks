058d3bb1b946edafa543ff7fc135e290b8d24479
Douglas Adamson <douglas.adamson@hc2158.local>
2017-07-28 14:06:23 -0600

    V2

--- quandlQuotes.R
+++ quandlQuotes.R
@@ -8,10 +8,17 @@ library(readr)
 # Get a quote based on ticker name and start and end date range
 #
 quandl <- function(ticker, start_date, end_date) {
+  # Construct the service call
   quandlArgs <- paste("start_date=", start_date, "&", "end_data=", end_date, "&ord=asc", sep="")
   tickerArg = paste(ticker, ".csv", sep="")
   quandlUrl <- "https://www.quandl.com/api/v3/datasets/WIKI/"
   quandlSendURL <- paste(quandlUrl, tickerArg, "?", quandlArgs, sep="")
+  
+  # Call the service returning a CSV
   quandlCSV <- read_csv(quandlSendURL, col_names=TRUE)
-  quandlCSV
+  
+  # Convert everything to Date objects
+  quandlCSV$Date <- as.Date(quandlCSV$Date, format = "%Y-%m-%d")
+  
+  return(quandlCSV)
 }
